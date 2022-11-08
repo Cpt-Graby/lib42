33c1582430351ead2584300663805fdcc2f11171
Alexis <alexisgonelle.mobile@gmail.com>
2022-11-08 17:25:18 +0100

    :Hello

--- srcs/ft_main/ft_putunnbr_fd.c
+++ srcs/ft_main/ft_putunnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 09:12:27 by agonelle          #+#    #+#             */
-/*   Updated: 2022/11/02 09:16:22 by agonelle         ###   ########.fr       */
+/*   Updated: 2022/11/08 15:22:59 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,7 @@
 
 void	ft_putunnbr_fd(unsigned int n, int fd)
 {
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-		ft_putnbr_fd(n, fd);
-	}
-	else if (n > 9)
+	if (n > 9)
 	{
 		ft_putnbr_fd((n) / 10, fd);
 		ft_putnbr_fd((n) % 10, fd);
